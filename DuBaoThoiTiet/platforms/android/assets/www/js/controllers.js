2e3ef7404041c3f96972883862d0a4a2cb627e83
SONY <SONY@SONY-PC>
2016-07-13 23:11:18 +0700

    Change

--- DuBaoThoiTiet/platforms/android/assets/www/js/controllers.js
+++ DuBaoThoiTiet/platforms/android/assets/www/js/controllers.js
@@ -8,9 +8,9 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function ($sco
 
 weatherApp.controller('forecastController', ['$scope', '$resource', '$stateParams', 'cityService', function ($scope, $resource, $stateParams, cityService) {
     $scope.city = cityService.city;
-    $scope.days = $stateParams.days || 2;
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } });
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, lang: 'vi', units: 'metric', APPID: 'efd929b8c471b2a11adee319ed694670' });
+    //$scope.days = $stateParams.days || 2;
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast/daily', { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } });
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2, lang: 'vi', units: 'metric', APPID: 'efd929b8c471b2a11adee319ed694670' });
 
     console.log($scope.weatherResult);
     $scope.convertToDate = function (dt) {
